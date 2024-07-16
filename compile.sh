44f5d3106cc619c7ba094fcf48d1b81e062dd85f
Mnnerva x <rndwnspam@gmail.com>
2024-07-16 15:35:33 +0000

    kernel: NGK Rebrand

--- compile.sh
+++ compile.sh
@@ -4,19 +4,19 @@
 PHONE="x3pro"
 ARCH="arm64"
 SUBARCH="arm64"
-DEFCONFIG=nogravity_defconfig
+DEFCONFIG="nogravity_defconfig"
 COMPILER=clang
 LINKER="lld"
-COMPILERDIR="/media/pierre/Expension/Android/PocoX3Pro/Kernels/Proton-Clang"
+COMPILERDIR="/workspace/kernel_xiaomi_vayu/proton-clang"
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 
 # Cleanup output
 rm -rf out/outputs/${PHONE}/*
 
 # Export shits
-export KBUILD_BUILD_USER=Pierre2324
-export KBUILD_BUILD_HOST=G7-7588
+export KBUILD_BUILD_USER=naZ
+export KBUILD_BUILD_HOST=RB-15
 
 # Speed up build process
 MAKE="./makeparallel"
@@ -28,17 +28,6 @@ cyan='\033[0;36m'
 yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
-
-Build () {
-PATH="${COMPILERDIR}/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-ARCH=${ARCH} \
-CC=${COMPILER} \
-CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=${COMPILERDIR}/bin/arm-linux-gnueabi- \
-LD_LIBRARY_PATH=${COMPILERDIR}/lib \
-Image.gz-dtb dtbo.img
-}
 
 Build_lld () {
 PATH="${COMPILERDIR}/bin:${PATH}" \
@@ -54,21 +43,8 @@ OBJCOPY=llvm-objcopy \
 OBJDUMP=llvm-objdump \
 STRIP=llvm-strip \
 ld-name=${LINKER} \
-KBUILD_COMPILER_STRING="Proton-Clang" \
+KBUILD_COMPILER_STRING="Proton Clang" \
 Image.gz-dtb dtbo.img
-}
-
-miui_fix_dimens() {
-    sed -i 's/<70>/<695>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
-    sed -i 's/<154>/<1546>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
-    sed -i 's/<70>/<695>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
-    sed -i 's/<154>/<1546>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
-}
-restore_dimens() {
-    sed -i 's/<695>/<70>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
-    sed -i 's/<1546>/<154>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
-    sed -i 's/<695>/<70>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
-    sed -i 's/<1546>/<154>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
 }
 
 # Make defconfig
@@ -94,22 +70,6 @@ then
     echo "Build failed"
 else
     echo "Build succesful"
-    mkdir out/outputs
-    mkdir out/outputs/${PHONE}
-    find out/arch/arm64/boot/dts/qcom/ -name '*.dtb' -exec cat {} + >out/outputs/${PHONE}/dtb
-    cp out/arch/arm64/boot/dtbo.img out/outputs/${PHONE}/dtbo.img
-    cp out/arch/arm64/boot/Image.gz out/outputs/${PHONE}/Image.gz
-    #MIUI dtbo
-    rm out/outputs/dtbo-miui.img
-    miui_fix_dimens
-    echo | Build_lld
-    if [ $? -ne 0 ]
-    then
-        rm out/outputs/dtbo-miui.img
-    else
-        cp out/arch/arm64/boot/dtbo.img out/outputs/dtbo-miui.img
-    fi
-    restore_dimens
 fi
 
 BUILD_END=$(date +"%s")
